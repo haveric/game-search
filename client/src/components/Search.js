3ee6ec7ab862710a4144872a643591d1cc93d90a
Ryan Breuer <breuerryan@gmail.com>
2018-10-25 08:35:35 -0500

    Fix image display, add loading state, and delay queries

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -12,9 +12,11 @@ class Search extends Component {
                 number_of_page_results: 0,
                 number_of_total_results: 0,
                 results: []
-            }
+            },
+            resultsLoading: false
         }
         this.setQuery = this.setQuery.bind(this);
+        this.queryDelay = null;
     }
   
 
@@ -23,19 +25,29 @@ class Search extends Component {
   }
 
   setQuery(e) {
+      var self = this;
       const query = e.target.value;
-      this.setState({
-        query: query,
-      });
 
-      fetch(`/games?q=${query}`)
-      .then(res => res.json())
-      .then(results => {
-        if (this.state.query === query) {  
-            this.setState({queryResults: results})
-        }
-        
+      self.setState({
+            query: query,
+            resultsLoading: true
         });
+
+      clearTimeout(self.queryDelay);
+
+      self.queryDelay = setTimeout(function() {
+        fetch(`/games?q=${query}`)
+        .then(res => res.json())
+        .then(results => {
+            if (self.state.query === query) {  
+                self.setState({
+                    queryResults: results,
+                    resultsLoading: false,
+                })
+            }
+            
+            });
+        }, 250);
   }
 
   render() {
@@ -48,11 +60,16 @@ class Search extends Component {
             <h1 className="Search__title">Search for a game:</h1>
             <input type="text" className="Search__input" onChange={ this.setQuery } value={ this.state.query } />
 
-            <div className="Search__results clearfix">
-                <p className="Search__results-count">Showing Results {pageResults} of {totalResults}</p>
-                <p className="Search__poweredby">Powered by <a href="https://www.giantbomb.com">Giantbomb</a></p>
-
+            <div className="Search__results">
+                <div class="clearfix">
+                    <p className="Search__results-count">Showing Results {pageResults} of {totalResults}</p>
+                    <p className="Search__poweredby">Powered by <a href="https://www.giantbomb.com">Giantbomb</a></p>
+                </div>
+                <div class="clearfix">
                 {
+                    this.state.resultsLoading ? 
+                        <div class="Search__loader">Loading...</div>
+                    :
                     results.map(game => (
                         <SearchResult 
                             key={game.id}
@@ -62,6 +79,7 @@ class Search extends Component {
                         />
                     ))
                 }
+                </div>
             </div>
         </div>
     );
